507f3945b2a50974f89729d9a6e3ad7529699bee
chounjae <blueskycuj@naver.com>
2025-02-06 17:08:52 +0900

    0.1.3

--- Reed/CUJO/urls.py
+++ Reed/CUJO/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('<int:dashboard_id>/', views.dashboard, name='dashboard'),
     path('logout/',views.logout_view,name='logout'),
     path('signup/',views.signup_view, name='signup'),
-    path('write/',views.write_dash, name='write')
+    path('write/',views.write_dash, name='write'),
+    path('<int:dashboard_id>/like/',views.like_view,name='likes'),
+    path('<int:dashboard_id>/dislike/',views.dislike_view,name='dislikes')
 ]
